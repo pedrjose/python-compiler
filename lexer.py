b3cc3d7ab69e1c3cd36faf8ab46224a6111b6686
pedro josé lima <pedrlimadev@gmail.com>
2024-06-07 22:22:05 -0300

    feat: Python Compiler Implements 'Interpreter'

--- lexer.py
+++ lexer.py
@@ -3,25 +3,25 @@ from errors import *
 DIGITS = '0123456789'
 
 class Position:
-		def __init__(self, idx, ln, col, fn, ftxt):
-				self.idx = idx
-				self.ln = ln
-				self.col = col
-				self.fn = fn
-				self.ftxt = ftxt
+	def __init__(self, idx, ln, col, fn, ftxt):
+		self.idx = idx
+		self.ln = ln
+		self.col = col
+		self.fn = fn
+		self.ftxt = ftxt
 
-		def advance(self, current_char=None):
-				self.idx += 1
-				self.col += 1
+	def advance(self, current_char=None):
+		self.idx += 1
+		self.col += 1
 
-				if current_char == '\n':
-						self.ln += 1
-						self.col = 0
+		if current_char == '\n':
+			self.ln += 1
+			self.col = 0
 
-				return self
+		return self
 
-		def copy(self):
-				return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+	def copy(self):
+		return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
 TT_INT			= 'INT'
 TT_FLOAT    = 'FLOAT'
@@ -34,88 +34,84 @@ TT_RPAREN   = 'RPAREN'
 TT_EOF			= 'EOF'
 
 class Token:
-		def __init__(self, type_, value=None, pos_start=None, pos_end=None):
-				self.type = type_
-				self.value = value
+	def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+		self.type = type_
+		self.value = value
 
-				if pos_start:
-					self.pos_start = pos_start.copy()
-					self.pos_end = pos_start.copy()
-					self.pos_end.advance()
+		if pos_start:
+			self.pos_start = pos_start.copy()
+			self.pos_end = pos_start.copy()
+			self.pos_end.advance()
 
-				if pos_end:
-					self.pos_end = pos_end
-		
-		def __repr__(self):
-				if self.value: return f'{self.type}:{self.value}'
-				return f'{self.type}'
-
-#######################################
-# LEXER
-#######################################
+		if pos_end:
+			self.pos_end = pos_end
+	
+	def __repr__(self):
+		if self.value: return f'{self.type}:{self.value}'
+		return f'{self.type}'
 
 class Lexer:
-		def __init__(self, fn, text):
-				self.fn = fn
-				self.text = text
-				self.pos = Position(-1, 0, -1, fn, text)
-				self.current_char = None
+	def __init__(self, fn, text):
+		self.fn = fn
+		self.text = text
+		self.pos = Position(-1, 0, -1, fn, text)
+		self.current_char = None
+		self.advance()
+	
+	def advance(self):
+		self.pos.advance(self.current_char)
+		self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+
+	def make_tokens(self):
+		tokens = []
+
+		while self.current_char != None:
+			if self.current_char in ' \t':
 				self.advance()
-		
-		def advance(self):
-				self.pos.advance(self.current_char)
-				self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
-
-		def make_tokens(self):
-				tokens = []
-
-				while self.current_char != None:
-						if self.current_char in ' \t':
-								self.advance()
-						elif self.current_char in DIGITS:
-								tokens.append(self.make_number())
-						elif self.current_char == '+':
-								tokens.append(Token(TT_PLUS, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '-':
-								tokens.append(Token(TT_MINUS, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '*':
-								tokens.append(Token(TT_MUL, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '/':
-								tokens.append(Token(TT_DIV, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '(':
-								tokens.append(Token(TT_LPAREN, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == ')':
-								tokens.append(Token(TT_RPAREN, pos_start=self.pos))
-								self.advance()
-						else:
-								pos_start = self.pos.copy()
-								char = self.current_char
-								self.advance()
-								return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
-
-				tokens.append(Token(TT_EOF, pos_start=self.pos))
-				return tokens, None
-
-		def make_number(self):
-				num_str = ''
-				dot_count = 0
+			elif self.current_char in DIGITS:
+				tokens.append(self.make_number())
+			elif self.current_char == '+':
+				tokens.append(Token(TT_PLUS, pos_start=self.pos))
+				self.advance()
+			elif self.current_char == '-':
+				tokens.append(Token(TT_MINUS, pos_start=self.pos))
+				self.advance()
+			elif self.current_char == '*':
+				tokens.append(Token(TT_MUL, pos_start=self.pos))
+				self.advance()
+			elif self.current_char == '/':
+				tokens.append(Token(TT_DIV, pos_start=self.pos))
+				self.advance()
+			elif self.current_char == '(':
+				tokens.append(Token(TT_LPAREN, pos_start=self.pos))
+				self.advance()
+			elif self.current_char == ')':
+				tokens.append(Token(TT_RPAREN, pos_start=self.pos))
+				self.advance()
+			else:
 				pos_start = self.pos.copy()
+				char = self.current_char
+				self.advance()
+				return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
 
-				while self.current_char != None and self.current_char in DIGITS + '.':
-						if self.current_char == '.':
-								if dot_count == 1: break
-								dot_count += 1
-								num_str += '.'
-						else:
-								num_str += self.current_char
-						self.advance()
+		tokens.append(Token(TT_EOF, pos_start=self.pos))
+		return tokens, None
 
-				if dot_count == 0:
-						return Token(TT_INT, int(num_str), pos_start, self.pos)
-				else:
-						return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
+	def make_number(self):
+		num_str = ''
+		dot_count = 0
+		pos_start = self.pos.copy()
+
+		while self.current_char != None and self.current_char in DIGITS + '.':
+			if self.current_char == '.':
+				if dot_count == 1: break
+				dot_count += 1
+				num_str += '.'
+			else:
+				num_str += self.current_char
+			self.advance()
+
+		if dot_count == 0:
+			return Token(TT_INT, int(num_str), pos_start, self.pos)
+		else:
+			return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
--- runner.py
+++ runner.py
@@ -1,12 +1,21 @@
 from lexer import *
 from parser import *
+from interpreter import *
 
 def run(fn, text):
-		lexer = Lexer(fn, text)
-		tokens, error = lexer.make_tokens()
-		if error: return None, error
-		
-		parser = Parser(tokens)
-		ast = parser.parse()
+	# Generate tokens
+	lexer = Lexer(fn, text)
+	tokens, error = lexer.make_tokens()
+	if error: return None, error
+	
+	# Generate AST
+	parser = Parser(tokens)
+	ast = parser.parse()
+	if ast.error: return None, ast.error
 
-		return ast.node, ast.error
+	# Run program
+	interpreter = Interpreter()
+	context = Context('<program>')
+	result = interpreter.visit(ast.node, context)
+
+	return result.value, result.error
